dc1645e093687b3146b32acff03cd13ec622cc4f
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2022-04-01 19:32:54 +0900

    Basic

---
+++ app/src/main/java/com/example/myfirstapp/Basics.kt
@@ -0,0 +1,9 @@
+package com.example.kotlibasics
+
+fun main() {
+//    var 과 val의 차리점은 var 변수는 중복 기재가 가능하지만 val은 안됨
+    var myName = "Frank"
+    myName = "Glaudia"
+    print("Hello " + myName)
+}
+
