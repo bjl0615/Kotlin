4d3675173e07564f301fc5d579f6af913ee99725
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2022-04-01 20:05:47 +0900

    Number

---
+++ app/src/main/java/com/example/myfirstapp/KotlinBascis.kt
@@ -0,0 +1,21 @@
+package com.example.kotlibasics
+
+fun main() {
+
+    // type String
+    val myName = "Frank"
+    // type int
+    var myAge = 12
+    print("Hello" + myName)
+
+//    Integer TYPES : Byte (8 bit), Short(16 bit)
+//    Int(32 bit) , Long (64 bit)
+    val myByte: Byte = 13
+    val myShort: Short = 125
+    val myInt: Int = 123123123
+    val myLong: Long = 39_812_309_487_120_3940
+
+    // Floating Point number Types : Float (32 bit), Double (64 bit)
+    val myFloat : Float = 13.37F
+    val myDouble : Double = 3.14159265358979323846
+}
