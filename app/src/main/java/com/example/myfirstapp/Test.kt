ef1174b4fe2375852ab644651c1eafe532ebba70
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2022-04-04 20:12:34 +0900

    test

---
+++ app/src/main/java/com/example/myfirstapp/Test.kt
@@ -0,0 +1,10 @@
+package com.example.kotlibasics
+
+fun main() {
+    // Strings
+    val myStr = "Hello World"
+    var firstCharInStr = myStr[0]
+    var lastCharInStr = myStr[myStr.length - 1]
+    var myLength = myStr.length
+    print("Fist character $firstCharInStr and the lesson of myStr is ${myStr.length}")
+}
