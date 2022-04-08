356bc1c670b7a183d1ef175cc985f03a56ab61ee
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2022-04-09 00:32:52 +0900

    class

--- app/src/main/java/com/example/myfirstapp/class.kt
+++ app/src/main/java/com/example/myfirstapp/class.kt
@@ -1,15 +1,36 @@
 package com.example.kotlibasics
 
 fun main() {
-    var lee = Person("Lee" , "bongjun")
-    var john = Person()
-    var johnPerterson = Person(lastName = "Peterson")
+    var Lee = Person("Lee", "Bong" , 24);
+    Lee.hobby = "skateboard"
+    Lee.stateHobby();
+    Lee.age = 25
+    println(Lee.age)
+    var john = Person();
+    john.hobby = "play video games"
+    john.stateHobby()
+//    var bong = Person(lastName = "jun");
 }
 
-class Person(firstName : String = "John", lastName: String = "Doe") {
+class Person (firstName : String = "Bong", lastName : String = "jun") {
+    // Member Variables - properties
+    var age : Int? = null
+    var hobby : String = "watch Netflix";
+    var firstName : String? = null
 
     init {
-        println("Initialized a new Person object with" +
-                "firstName = $firstName and lastName = $lastName")
+        this.firstName = firstName
+        println("firstName = $firstName LastName = $lastName");
+    }
+
+    //Member secondary Constructor
+    constructor(firstName : String, lastName: String, age: Int)
+                : this(firstName,lastName) {
+                 this.age = age
+            }
+
+    //Member functions - Methods
+    fun stateHobby() {
+        println("$firstName \'s hobby us $hobby")
     }
 }
